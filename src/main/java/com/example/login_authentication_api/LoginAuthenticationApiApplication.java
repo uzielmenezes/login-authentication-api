89e454ede4aa45ce0d9b129f82ef161dff947727
Uziel <uzielcivil@gmail.com>
2024-08-02 14:26:49 -0300

    feat: initializing + initial settings

---
+++ src/main/java/com/example/login_authentication_api/LoginAuthenticationApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.login_authentication_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginAuthenticationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginAuthenticationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/login_authentication_api/LoginAuthenticationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.login_authentication_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAuthenticationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
