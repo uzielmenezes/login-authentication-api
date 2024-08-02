420a7ec943c8e8e40fd27ed262ca74786a7bf32b
Uziel <uzielcivil@gmail.com>
2024-08-02 14:29:06 -0300

    feat: class so spring access registered users

---
+++ src/main/java/com/example/login_authentication_api/infra/security/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.example.login_authentication_api.infra.security;
+
+import java.util.ArrayList;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import com.example.login_authentication_api.domain.user.User;
+import com.example.login_authentication_api.repositories.UserRepository;
+
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = this.userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(),
+                new ArrayList<>());
+    }
+
+}
